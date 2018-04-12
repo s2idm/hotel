c5d574726c94dbf1b8949af175403e63e5a7c0e5
avomark <s2idm.azrou@gmail.com>
2018-04-12 17:39:09 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# hotel
+# Heroku Django Starter Template
+
+Project starter template for Django 1.11
+
+Requirements
+
+    Python 3.5+
+    Postgres 9.4+
+
+## Creating Your Project
+
+Using this template to create a new Django app is easy::
+
+    $ pip install django
+    $ django-admin.py startproject --template=https://github.com/bahattincinic/heroku-django-template/archive/master.zip --name=Procfile helloworld --extension=py,md
+
+
+## Deployment to Heroku
+
+    $ git init
+    $ git add -A
+    $ git commit -m "Initial commit"
+
+    $ heroku create
+    $ git push heroku master
+
+    $ heroku run python manage.py migrate
+
+## Existing Heroku Project
+
+    $ heroku login
+    $ heroku git:remote -a project_name
+    $ git push heroku master
+    $ heroku run python manage.py migrate
---
+++ azrou/settings/base.py
@@ -0,0 +1,148 @@
+from os.path import abspath, dirname, join, normpath
+from sys import path
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
+project_root = normpath(join(BASE_DIR, 'azrou'))
+APPS = normpath(join(project_root, 'apps'))
+path.insert(1, APPS)
+path.insert(2, project_root)
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '@ap-7bp9)i7(0+cy-(0&!r&yn561yx+ycp_&#&5nmql1(^o9w9'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+DJANGO_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+LOCAL_APPS = []
+
+THIRD_PARTY_APPS = [
+    'compressor'
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'azrou.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [join(project_root, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'azrou.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'azrou',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.11/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = normpath(join(project_root, 'static'))
+
+STATICFILES_DIRS = (
+    normpath(join(project_root, 'assets')),
+)
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = normpath(join(project_root, 'media'))
+
+
+# Fixtures
+FIXTURE_DIRS = (
+    normpath(join(project_root, 'fixtures')),
+)
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder'
+)
