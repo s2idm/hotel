1d03df68a34d2b75206a4373255970bd99918bfc
avomark <s2idm.azrou@gmail.com>
2018-04-18 01:56:49 +0200

    Initial commit

--- azrou/settings/database.py
+++ azrou/settings/database.py
@@ -2,10 +2,10 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd4nm4nprml21rg',
-        'USER': 'iiynkdqbvhpjll',
-        'PASSWORD': '97ce0c517c16826d2dbe7b8b7a2838535c1ab53c8cb57ec289071375e79ca4b6',
-        'HOST': 'ec2-54-204-21-226.compute-1.amazonaws.com',
+        'NAME': 'd6mdnij1t4c73t',
+        'USER': 'izoehoxygwgprl',
+        'PASSWORD': '41777a77df79c2719bba13535acc91066763545f2801f8c2b0fbcce20c709a89',
+        'HOST': 'ec2-54-225-96-191.compute-1.amazonaws.com',
         'PORT': '5432',
     },
 }
