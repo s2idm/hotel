c0fd0b68de2ec1f11f0f413e00fb9ba6e7d932e7
avomark <s2idm.azrou@gmail.com>
2018-04-18 02:33:23 +0200

    Initial commit

--- azrou/settings/base.py
+++ azrou/settings/base.py
@@ -100,10 +100,10 @@ WSGI_APPLICATION = 'azrou.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ddlvr1d5h5dhmb',
-        'USER': 'ypcyjgiprkejjt',
-        'PASSWORD': 'd9bf161c36b543dd88e89fb752b0b239d32f2e2728c3edd3814f4b3377901e5a',
-        'HOST': 'ec2-54-163-240-54.compute-1.amazonaws.com',
+        'NAME': 'd5fbfdbf3lueip',
+        'USER': 'crnzofygxzpgpj',
+        'PASSWORD': '8e5ed8c2fb55ed749f0a9a9ef1bd32869ba4c68413789b56efd7afe20e9443b7',
+        'HOST': 'ec2-54-235-193-34.compute-1.amazonaws.com',
         'PORT': '5432',
     },
 }
