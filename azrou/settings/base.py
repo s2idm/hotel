264a7fb08d3e673972e424c983db840c2dd197b9
avomark <s2idm.azrou@gmail.com>
2018-04-18 02:26:30 +0200

    Initial commit

--- azrou/settings/base.py
+++ azrou/settings/base.py
@@ -99,9 +99,13 @@ WSGI_APPLICATION = 'azrou.wsgi.application'
 #}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ddlvr1d5h5dhmb',
+        'USER': 'ypcyjgiprkejjt',
+        'PASSWORD': 'd9bf161c36b543dd88e89fb752b0b239d32f2e2728c3edd3814f4b3377901e5a',
+        'HOST': 'ec2-54-163-240-54.compute-1.amazonaws.com',
+        'PORT': '5432',
+    },
 }
 
 # Password validation
